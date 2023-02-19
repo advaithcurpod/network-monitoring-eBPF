c4df85e9077e28e5ec0ab907980c5722ac3d7416
Sidhub723 <sidbhat.211ee151@nitk.edu.in>
2023-02-19 20:15:05 +0530

    First commit of program

--- mydropall.py
+++ mydropall.py
@@ -1,6 +1,6 @@
 from bcc import BPF 
 from time import sleep
-
+import os
 
 prog = """
 #define SEC(NAME) __attribute__((section(NAME), used))
@@ -9,33 +9,41 @@ SEC("prog")
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/udp.h>
-
 #include <linux/in.h>
 
-int dropall(struct CTXTYPE *ctx) 
+
+int dropall(struct xdp_md *ctx) 
 {
+    void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
-    void *data  = (void *)(long)ctx->data;
-    struct ethaddr *eth = data;
-    if(eth + 1 > data_end){
+
+    if (data + sizeof(struct ethhdr) > data_end) {
         return XDP_PASS;
     }
+
+    struct ethhdr *eth = data;
+    if (eth + 1 > (struct ethhdr *)data_end) {
+        return XDP_PASS;
+    }
+
     return XDP_DROP;
+
 }
 
 """
 
-device = "wlp1s0"
+device = "eth0"
 
 b =BPF(text=prog)
 fn = b.load_func("dropall",BPF.XDP)
-b.attach_xdp(device,0)
+b.attach_xdp(device,fn)
+
 
 while True:
     sleep(1.5)
     try:
         b.trace_print()
     except KeyboardInterrupt:
-        b.remove_xdp(device,0)
+        b.remove_xdp(device)
         exit()
 
